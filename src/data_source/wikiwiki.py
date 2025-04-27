a346287e414aa62c3358edb6aa54011d32c9d49a
d2verb <licprog73@gmail.com>
2025-04-27 23:41:05 +0900

    Refactor parser code

--- src/data_source/wikiwiki.py
+++ src/data_source/wikiwiki.py
@@ -11,11 +11,31 @@ class WikiWikiDataSource:
     BASE_URL = "https://wikiwiki.jp/w-w/"
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
-    def get_resonator_by_name(self, name: str) -> Resonator | None:
+    def get_resonator_by_name(self, name: str) -> Resonator:
+        weapon_type = None
+        attribute = None
+
+        # parse character page
+        wikitext = self._get_page_source(name)
+        if wikitext is None:
+            raise DataParsingError(f"Failed to get resonator data for {name}")
+
+        for line in wikitext.splitlines():
+            weapon_type = (
+                self._parse_weapon_type(line) if weapon_type is None else weapon_type
+            )
+            attribute = self._parse_attribute(line) if attribute is None else attribute
+
+        if not all([weapon_type, attribute]):
+            raise DataParsingError(f"Failed to parse resonator data for {name}")
+
+        return Resonator(name=name, weapon_type=weapon_type, attribute=attribute)
+
+    def _get_page_source(self, page: str) -> str | None:
         url = f"{self.BASE_URL}::cmd/source"
         r = httpx.get(
             url,
-            params={"page": name},
+            params={"page": page},
             headers={"User-Agent": self.USER_AGENT},
         )
 
@@ -27,21 +47,16 @@ class WikiWikiDataSource:
         soup = BeautifulSoup(r.text, "html.parser")
         wikitext = soup.select_one("#source > code").text
 
-        weapon_type = None
-        attribute = None
+        return wikitext
 
-        for line in wikitext.splitlines():
-            # attribute
-            m = re.search(r"\|[^|]*\|属性\|[^;]*;([^|]+)\|?", line)
-            if m:
-                attribute = Attribute.from_ja(m.group(1).strip())
+    def _parse_attribute(self, line: str) -> Attribute | None:
+        m = re.search(r"\|[^|]*\|属性\|[^;]*;([^|]+)\|?", line)
+        if m:
+            return Attribute(m.group(1).strip())
+        return None
 
-            # weapon type
-            m = re.search(r"\|~\|武器\|[^;]*;([^|]+)\|?", line)
-            if m:
-                weapon_type = WeaponType.from_ja(m.group(1).strip())
-
-        if weapon_type is None or attribute is None:
-            raise DataParsingError(f"Failed to parse resonator data for {name}")
-
-        return Resonator(name=name, weapon_type=weapon_type, attribute=attribute)
+    def _parse_weapon_type(self, line: str) -> WeaponType | None:
+        m = re.search(r"\|~\|武器\|[^;]*;([^|]+)\|?", line)
+        if m:
+            return WeaponType(m.group(1).strip())
+        return None
